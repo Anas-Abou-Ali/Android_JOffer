f75803bbc2bc8865e1a6d03e26aac5bf6fafeb8c
fatimazahra-azennag <58732724+fatimazahra-azennag@users.noreply.github.com>
2021-06-01 17:58:35 +0200

    plusieurs updates

---
+++ app/src/main/java/com/android/joffer/model/Image.java
@@ -0,0 +1,13 @@
+package com.example.joffre.model;
+
+public class Image {
+    private String value;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
---
+++ app/src/main/java/com/android/joffer/model/User.java
@@ -0,0 +1,76 @@
+package com.example.joffre.model;
+
+public class User {
+    private int id_user;
+    private String first_name;
+    private String last_name;
+    private String email;
+    private String password;
+    private String adresse;
+    private String phone;
+    private int id_rate;
+
+    public int getId_user() {
+        return id_user;
+    }
+
+    public void setId_user(int id_user) {
+        this.id_user = id_user;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public int getId_rate() {
+        return id_rate;
+    }
+
+    public void setId_rate(int id_rate) {
+        this.id_rate = id_rate;
+    }
+}
